f94b40284eb005269ede72fdc80fdd8481ab2581
vishal lokhande <lvishal108@gmail.com>
2024-10-23 14:49:49 +0000

    Render options Api , Api se data laya

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
 console.log("Connected");
+
+/* breeds/list/all = All breeds 
+
+breed/{breedName}/images/random
+*/
+
+//API
+const selectE1 = document.querySelector("select");
+
+console.log("start");
+const BASE_URL = `https://dog.ceo/api/`;
+
+function getDogBreed() {
+  return fetch(`${BASE_URL}breeds/list/all `)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(Object.keys(data.message));
+      return Object.keys(data.message);
+    })
+    .catch((error) => console.log(error));
+}
+
+console.log("end");
+function RenderOption() {
+  getDogBreed().then((data) => {
+    const fragment = document.createDocumentFragment();
+    for (let breed of data) {
+      const option = document.createElement("option");
+      option.textContent = breed;
+      fragment.appendChild(option);
+    }
+    selectE1.appendChild(fragment);
+  });
+}
+RenderOption();
