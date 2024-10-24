767ae9829cc51907c69fb612bb29d823ad9ec298
vishal lokhande <lvishal108@gmail.com>
2024-10-24 07:46:47 +0000

    H.W

--- src/main.js
+++ src/main.js
@@ -6,22 +6,20 @@ breed/{breedName}/images/random
 */
 
 //API
-const selectE1 = document.querySelector("select");
+const selectEl = document.querySelector("select");
 
-console.log("start");
 const BASE_URL = `https://dog.ceo/api/`;
 
 function getDogBreed() {
   return fetch(`${BASE_URL}breeds/list/all `)
     .then((res) => res.json())
     .then((data) => {
-      console.log(Object.keys(data.message));
+      // console.log(Object.keys(data.message));
       return Object.keys(data.message);
     })
     .catch((error) => console.log(error));
 }
 
-console.log("end");
 function RenderOption() {
   getDogBreed().then((data) => {
     const fragment = document.createDocumentFragment();
@@ -30,7 +28,30 @@ function RenderOption() {
       option.textContent = breed;
       fragment.appendChild(option);
     }
-    selectE1.appendChild(fragment);
+    selectEl.appendChild(fragment);
   });
 }
 RenderOption();
+
+
+// H.W
+
+console.log(selectEl);
+
+let a= selectEl.value;
+console.log(a);
+
+selectEl.addEventListener('change', function() {
+    const selectedOption = selectEl.value;
+    console.log(selectedOption); 
+});
+
+
+// console.log(RenderOption())
+//console.log(data);
+//console.log(RenderOption)
+// console.log(a)
+// console.log(selectE1);
+// let a = document.getElementsByTagName("option");
+// console.log(a);
+
